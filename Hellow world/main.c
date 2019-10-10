7cbf780074f5ac14e80dc77e8e02a3b539601553
Gordon777 <goydon6a@gmail.com>
2019-10-10 15:46:55 +0800

    new
    :))

---
+++ Hellow world/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char str[100];
+    int i;
+
+    printf("Enter a valus: ");
+
+    scanf("%d",&i);
+
+    printf("\nYou entered: %d",i,str);
+
+    return 0 ;
+
+
+}
